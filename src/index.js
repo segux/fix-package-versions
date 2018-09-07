54b806f6b6ecdb0ba414697a8d89aafb640fa1af
segux <josesegurag90@gmail.com>
2018-09-07 13:55:13 +0200

    First commit

---
+++ src/index.js
@@ -0,0 +1,45 @@
+const editJsonFile = require('edit-json-file');
+const isObject = require('./utils/is-object');
+const Prompt = require('prompt-base');
+
+const versionSpecialCharacter = new RegExp(/^[^<>~]/);
+
+const packageJsonFile = editJsonFile('./fake-package.json', { autosave: true });
+
+const devDependencies = packageJsonFile.get('devDependencies');
+const dependencies = packageJsonFile.get('dependencies');
+
+console.log('----------- Checking devDependencies versions -------------');
+const fixVersion = (namespace, dependencies) => {
+  if (!isObject(dependencies)) return;
+  Object.keys(dependencies).forEach(dependency => {
+    var version = dependencies[dependency];
+    if (versionSpecialCharacter.test(version)) {
+      console.log('dependency', dependency, version);
+      setDependency(namespace, dependency, version);
+    }
+  });
+};
+const setDependency = (namespace, dependency, version) => {
+  const nodeModulesPackage = editJsonFile(`./node_modules/${dependency}/package.json`);
+  const fixedVersion = nodeModulesPackage.get('version');
+  if (fixedVersion && fixedVersion <= version) {
+    console.log(`------ Setting ${namespace}.${dependency}: ${fixedVersion} ---------- `);
+    packageJsonFile.set(`${namespace}.${dependency}`, fixedVersion);
+  }
+};
+
+// prompts
+const areYouSurePrompt = new Prompt({
+  name: 'areYouSure',
+  message: 'With this action you will make versions fixed with the latest package version installed. Are you sure? Y/n'
+});
+// areYouSurePrompt.run().then(answer => {
+//   if (!answer) return;
+//   if (answer.toLowerCase() === 'y') {
+//     fixVersion('devDependencies', devDependencies);
+//     fixVersion('dependencies', dependencies);
+//   }
+// });
+fixVersion('devDependencies', devDependencies);
+fixVersion('dependencies', dependencies);
