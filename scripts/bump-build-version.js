d7a1c77c35163a60b2231d9fcfeee1a5d7d96d27
segux <josesegurag90@gmail.com>
2018-10-24 09:56:29 +0200

    Add travis CI and bump build version

---
+++ scripts/bump-build-version.js
@@ -0,0 +1,24 @@
+const packageJson = require( '../package.json' );
+const exec = require( 'child_process' ).exec;
+const travisBuildNumber = process.env.TRAVIS_BUILD_NUMBER;
+let buildVersion = 0;
+console.log( travisBuildNumber );
+if ( travisBuildNumber ) {
+  buildVersion = parseInt( travisBuildNumber );
+}
+
+let newVersion = packageJson.version.split( '.' ).map( ( versionNumber, index ) => {
+  if ( index === 2 ) {
+    return ( buildVersion );
+  } else {
+    return versionNumber;
+  }
+} ).join( '.' );
+
+exec( `npm version ${ newVersion }  --no-git-tag-version --yes`, null, ( error, stdout, stderr ) => {
+  if ( error ) {
+    console.error( `exec error: ${ error }` );
+  }
+  console.log( stdout );
+  console.log( `Bumped correctly build version ${ newVersion }` );
+} );
